444403400afe2c703e15c43dbc5d66b8b1d23c9c
platinum360 <aditya851999@gmail.com>
2026-04-05 21:37:40 +0530

    Fix: Completely remove career timeline and animations for mobile

--- src/components/utils/GsapScroll.ts
+++ src/components/utils/GsapScroll.ts
@@ -121,16 +121,17 @@ export function setCharTimeline(
 }
 
 export function setAllTimeline() {
-  const careerTimeline = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".career-section",
-      start: "top 30%",
-      end: "100% center",
-      scrub: true,
-      invalidateOnRefresh: true,
-    },
-  });
   if (window.innerWidth > 1024) {
+    const careerTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".career-section",
+        start: "top 30%",
+        end: "100% center",
+        scrub: true,
+        invalidateOnRefresh: true,
+      },
+    });
+
     careerTimeline
       .fromTo(
         ".career-timeline",
@@ -153,28 +154,18 @@ export function setAllTimeline() {
           duration: 0.1,
         },
         0
+      )
+      .fromTo(
+        ".career-info-box",
+        { opacity: 0 },
+        { opacity: 1, stagger: 0.1, duration: 0.5 },
+        0
       );
-  }
 
-  careerTimeline.fromTo(
-    ".career-info-box",
-    { opacity: 0 },
-    { opacity: 1, stagger: 0.1, duration: 0.5 },
-    0
-  );
-
-  if (window.innerWidth > 1024) {
     careerTimeline.fromTo(
       ".career-section",
       { y: 0 },
       { y: "20%", duration: 0.5, delay: 0.2 },
-      0
-    );
-  } else {
-    careerTimeline.fromTo(
-      ".career-section",
-      { y: 0 },
-      { y: 0, duration: 0.5, delay: 0.2 },
       0
     );
   }
